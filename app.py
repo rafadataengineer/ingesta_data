4fb0607f77c98bba4b7e00f91a122cc9c350d3b0
rafadataengineer <rafadataengineer@gmail.com>
2023-01-08 20:37:50 -0500

    Se agrega el script app.py que permite conectarse a la base de datos, obtener el archivo y almacenar el archivo en base de datos

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+from pymongo import MongoClient
+import csv
+
+app = Flask(__name__)
+
+# Conexion a la base de datos MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client("postcodes")
+
+@app.route("/upload", methods=["POST"])
+def upload_csv():
+    #Obtener el archivo csv enviado en la peticion
+    file = request.files["file"]
+
+    #Leer el archivo y almacenar los datos en la base de datos
+    reader = csv.DictReader(file)
+    for row in reader:
+        db.coordinates.insert_one(row)
+
+    return "Archivo csv almacenado en la base de datos"
+
+
+if __name__ == "__main__":
+    app.run()
